85539bf52de23bd94eded8ecb676dbb4491326a8
Martin Sperl <kernel@martin.sperl.org>
2018-10-28 18:20:41 +0000

    allow the gcc version to be parametrized (if allready installed)
    Signed-off-by: Martin Sperl <kernel@martin.sperl.org>

--- build_check_mk.sh
+++ build_check_mk.sh
@@ -3,6 +3,7 @@
 VERSION="1.5.0p2"
 SNAP7_VERSION="1.3.0"
 FAST_BUILD=0
+GCC_VERSION=${GCC_VERSION:-"8.1.0"}
 
 if [ $# -gt 0 ]; then
   VERSION="$1"
@@ -11,14 +12,22 @@ fi
 echo "building Check_MK ${VERSION}..."
 
 # Get compiler with support for C++17 language features
-if [ ! -d "/opt/gcc-8.1.0" ] ; then
+# note that livestatus is producing issues with 8.1.0, but let us keep it for now...
+if [ ! -d "/opt/gcc-$GCC_VERSION" ] ; then
     wget -qO- https://bitbucket.org/sol_prog/raspberry-pi-gcc-binary/raw/bad4e5d14042cdad8f081119e5808fa3798217f6/gcc-8.1.0.tar.bz2?at=master | tar jx
     mv gcc-8.1.0 /opt
-    update-alternatives --install /usr/bin/gcc gcc /opt/gcc-8.1.0/bin/gcc-8.1.0 10
-    update-alternatives --install /usr/bin/g++ g++ /opt/gcc-8.1.0/bin/g++-8.1.0 10
+    GCC_VERION=8.1.0
 fi
-update-alternatives --set gcc /opt/gcc-8.1.0/bin/gcc-8.1.0
-update-alternatives --set g++ /opt/gcc-8.1.0/bin/g++-8.1.0
+
+# setting up defaults
+update-alternatives --install /usr/bin/gcc gcc /opt/gcc-$GCC_VERSION/bin/gcc-$GCC_VERSION 10
+update-alternatives --install /usr/bin/g++ g++ /opt/gcc-$GCC_VERSION/bin/g++-$GCC_VERSION 10
+update-alternatives --set gcc /opt/gcc-$GCC_VERSION/bin/gcc-$GCC_VERSION
+update-alternatives --set g++ /opt/gcc-$GCC_VERSION/bin/g++-$GCC_VERSION
+
+# add the gcc bin to path
+PATH=/opt/gcc-$GCC_VERSION/bin:$PATH
+export PATH
 
 # get check_mk sources and build dependencies
 apt-get -y install git make debhelper flex build-essential libssl1.0-dev libboost-dev libboost-all-dev libncurses5-dev libgd-dev libglib2.0-dev libgnutls28-dev default-libmysqlclient-dev libpango1.0-dev libperl-dev libxml2-dev libsqlite3-dev uuid-dev apache2-dev libpcap-dev libgsf-1-dev libkrb5-dev tk-dev
